fb5c38c41647b89c9b2787e3de301b7c25fd5027
kpunkay <korteg8@gmail.com>
2026-03-05 14:48:02 -0600

    Save changes to homework 4 file

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+"""
+Meter_Hw4 Package - Artificial Neural Network and Machine Learning Models
+
+This package provides tools for data preprocessing and machine learning model building.
+"""
+
+from src.data_preprocess import DataPreprocessing
+from src.model_builder import ModelBuilder
+
+__all__ = ['DataPreprocessing', 'ModelBuilder']
